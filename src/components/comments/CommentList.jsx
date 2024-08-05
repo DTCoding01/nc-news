01fe09fc9d335224ec94b12e64d79eaeb421f241
Daniel Taylor <danieltaylor@Daniels-MacBook-Pro.local>
2024-08-05 19:49:14 +0100

    added comments for articles with styling

---
+++ src/components/comments/CommentList.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { getCommentsByArticleId } from "../../../api";
+import CommentCard from "./CommentCard";
+import '../../css/components/CommentsList.scss'
+
+export default function CommentList({ articleId }) {
+  const [comments, setComments] = useState([]);
+  useEffect(() => {
+    if (articleId) {
+      getCommentsByArticleId(articleId)
+        .then((comments) => {
+          setComments(comments);
+        })
+        .catch((error) => {
+          console.error("Error fetching comments:", error);
+        });
+    }
+  }, [articleId]);
+
+  return (
+    <section className="comments-list">
+      <p className="comments-header">Comments</p>
+      <ul>
+        {comments.map((comment) => {
+          return (
+            <li className="article-list-item" key={comment.comment_id}>
+              <CommentCard comment={comment} />
+            </li>
+          );
+        })}
+      </ul>
+    </section>
+  );
+}
